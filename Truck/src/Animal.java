d7c7c07fa5c9b9aa49ac53cb46d9707a3da332f3
srinivas <bandasrinivas93@gmail.com>
2023-06-07 16:29:04 +0530

    latest method

---
+++ Truck/src/Animal.java
@@ -0,0 +1,18 @@
+
+public class Animal {
+	public void m1() {
+		System.out.println("hai");
+	}
+	public void m2() {
+		System.out.println("hello");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Animal a = new Animal();
+		a.m1();
+		a.m2();
+
+	}
+
+}
